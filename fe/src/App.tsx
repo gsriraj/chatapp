886d2a0de69a1939de1f64ca5cf70742a06abf9b
Sriraj G <srirajg@deloitte.com>
2020-09-11 10:25:59 +0530

    test

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -7,11 +7,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    <div className="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-      Hellooo world!
+    <div className="container">
+      <div className="row">
+        <div className="col-4" style={{background:"black"}}>
+          1 of 2
+          </div>
+        <div className="col-8" style={{background:"blue"}}>
+          2 of 2
+          </div>
+      </div>
     </div>
   );
 }
